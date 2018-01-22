37e6ecbd8da5111a8776e086545b520b89bf267a
Viatcheslav Ivanov <slava.eth@gmail.com>
2018-01-22 01:16:08 -0800

    smart

--- client.js
+++ client.js
@@ -4,47 +4,25 @@ const smi = require('node-nvidia-smi')
 const UPDATE_INTERVAL = 5 * 1000 // 5sec
 
 const rig = {
-  name: process.env.RIG_NAME,
+  name: process.env.RIG_NAME || process.argv[2],
   profitability: process.env.RIG_PROFITABILITY,
 }
-let interval
-let ws
-let handlingError = false
 
-const connect = () => {
-  const handleError = () => {
-    if (!handlingError) {
-      console.log('disconnected')
-      handlingError = true
-      clearInterval(interval)
-      setTimeout(() => {
-        handlingError = false
-        connect()
-      }, 5000)
-    }
-  }
+console.log('connecting...')
+const ws = new WebSocket('ws://77e3e5f8.ngrok.io')
+ws.on('open', () => {
+  console.info('connected to farm')
+  ws.send(JSON.stringify({ type: 'CONNECTION_SUCCESS', payload: { rig } }))
 
-  console.log('connecting...')
-  ws = new WebSocket('ws://77e3e5f8.ngrok.io')
-  ws.on('open', () => {
-    console.info('connected to farm')
-    ws.send(JSON.stringify({ type: 'CONNECTION_SUCCESS', payload: { rig } }))
+  interval = setInterval(() => {
+    // get system data
+    smi((err, data) => {
+      if (err) {
+        ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_FAILURE', payload: { rig, err } }))
+        console.warn(err)
+      }
 
-    interval = setInterval(() => {
-      // get system data
-      smi((err, data) => {
-        if (err) {
-          ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_FAILURE', payload: { rig, err } }))
-          console.warn(err)
-        }
-
-        ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_SUCCESS', payload: { rig, data: data.nvidia_smi_log }}))
-      })
-    }, UPDATE_INTERVAL)
-  })
-
-  ws.on('error', () => handleError())
-  ws.on('close', () => handleError())
-}
-
-connect()
+      ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_SUCCESS', payload: { rig, data: data.nvidia_smi_log }}))
+    })
+  }, UPDATE_INTERVAL)
+})
