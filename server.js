32be9937efa519d3ed3a866841b68995e11e0fd2
Viatcheslav Ivanov <slava.eth@gmail.com>
2018-01-21 21:40:49 -0800

    commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const WebSocket = require('ws')
 const twilio = require('twilio')(
   process.env.TWILIO_ACCOUNT_SID,
@@ -9,16 +10,19 @@ const { nicehashApi } = require('./utils/api')
 const BTC_WALLET = process.env.BTC_WALLET
 const ALERT_THRESHOLD = 5 * 60 * 1000 // 5min
 const UPDATE_INTERVAL = 1 * 3 * 1000 // 3sec
-const NICEHASH_UPDATE_INTERVAL = 1 * 60 * 1000 // 1min
+const NICEHASH_FETCH_INTERVAL = 1 * 60 * 1000 // 1min
 
 const stringifyProfitabilityInBTC = profitability => `${profitability.toFixed(10)} BTC/day`
 const stringifyProfitabilityInMilliBTC = profitability => `${(profitability * 1000).toFixed(7)} mBTC/day`
-const stringifyProfitability = stringifyProfitabilityInMilliBTC
+const stringifyBTCInMilliBTC = btc => `${(btc * 1000).toFixed(7)} mBTC`
 
-let profitability = 0.0
+let nicehashStats = {
+  profitability: 0.0,
+  balance: 0.0,
+}
 const rigs = {}
 
-const wss = new WebSocket.Server({ port: 9090 })
+const wss = new WebSocket.Server({ port: 9090, clientTracking: true })
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     const { type, payload: { rig, data } } = JSON.parse(message)
@@ -27,7 +31,8 @@ wss.on('connection', (ws) => {
       case 'CONNECTION_SUCCESS':
         console.log(`Rig ${rig.name} connected`)
         rigs[rig.name] = {
-          ...rig,
+          name: rig.name,
+          profitability: parseFloat(rig.profitability),
           gpus: [],
         }
         break
@@ -37,13 +42,33 @@ wss.on('connection', (ws) => {
         break
 
       case 'LOAD_SYSTEM_DATA_SUCCESS':
-        rigs[rig.name].gpus = [{
-          name: data.gpu.product_name,
-          usage: data.gpu.utilization.gpu_util,
-          temperature: data.gpu.temperature.gpu_temp,
-          power: data.gpu.power_readings.power_draw,
-        }]
-        // console.log(JSON.stringify(payload, null, '\t'))
+        rigs[rig.name].updatedAt = moment(data.timestamp)
+        if (Array.isArray(data.gpu)) {
+          rigs[rig.name].gpus = data.gpu.map(gpu => ({
+            pci: gpu.pci.pci_bus,
+            name: gpu.product_name,
+            usage: gpu.utilization.gpu_util,
+            temperature: gpu.temperature.gpu_temp,
+            power: {
+              draw: gpu.power_readings.power_draw,
+              limit: gpu.power_readings.power_limit,
+            },
+            fan: gpu.fan_speed,
+          }))
+        } else {
+          rigs[rig.name].gpus = [{
+            pci: data.gpu.pci.pci_bus,
+            name: data.gpu.product_name,
+            usage: data.gpu.utilization.gpu_util,
+            temperature: data.gpu.temperature.gpu_temp,
+            power: {
+              draw: data.gpu.power_readings.power_draw,
+              limit: data.gpu.power_readings.power_limit,
+            },
+            fan: data.gpu.fan_speed,
+          }]
+        }
+        // console.log(JSON.stringify(data, null, '\t'))
         break
 
       default:
@@ -60,16 +85,21 @@ const clearScreen = () => {
   readline.clearScreenDown(process.stdout)
 }
 
-const calcProfitability = async () => {
+const fetchNicehashStats = async () => {
   const { current } = await nicehashApi({
 		method: 'stats.provider.ex',
-		addr: BTC_WALLET
+		addr: BTC_WALLET,
 	})
 
 	const activeAlgorithms = current.filter(x => x.data[0].a)
 	const totalProfitability = activeAlgorithms.reduce((acc, x) => acc + x.profitability * x.data[0].a, 0)
 
-  return parseFloat(totalProfitability.toFixed(10))
+  const balance = current.reduce((acc, x) => acc + parseFloat(x.data[1]), 0.0)
+
+  return {
+    profitability: parseFloat(totalProfitability.toFixed(10)),
+    balance: parseFloat(balance.toFixed(10)),
+  }
 }
 
 const calcMinimumProfitability = () => {
@@ -85,72 +115,35 @@ const sendSms = message => (
 )
 
 const printFarmStats = () => {
+  const { balance, profitability } = nicehashStats
   const minimumProfitability = calcMinimumProfitability()
 
-  console.log('Mining Farm: Happy Bit')
+  console.log('Mining Farm: Happy Bit\n')
   let t = new Table()
   // total gpus
+  t.cell('Balance', stringifyBTCInMilliBTC(balance))
   t.cell('Profitability', stringifyProfitabilityInMilliBTC(profitability))
   t.cell('Min Profitability', stringifyProfitabilityInMilliBTC(minimumProfitability))
-  // t.cell('Price, USD', product.price, Table.number(2))
   t.newRow()
   console.log(t.toString())
 }
 
-let alertThresholdStartedAt
-
-const main = async () => {
-  clearScreen()
-  printFarmStats()
-
-  const minimumProfitability = calcMinimumProfitability()
-
-  // profitability
-  // try {
-  //   // totalProfitability += profitability
-  //   // const averageProfitability = totalProfitability / counter
-  //
-	// 	if (!profitability) {
-  //     const message = 'ZHOPA! No miners are working!'
-  //     // sendSms(message)
-  //
-  //     console.log('ZHOPA! No miners are working!')
-	// 		return
-	// 	}
-  //
-	// 	if (profitability < minimumProfitability) {
-  //     const message = `ZHOPA! Farm profitability is LOW! ${stringifyProfitability(profitability)} of ${stringifyProfitability(MIN_PROFITABILITY_IN_BTC)}`
-  //
-  //     alertThresholdStartedAt = alertThresholdStartedAt || Date.now()
-  //     if (Date.now() - alertThresholdStartedAt > ALERT_THRESHOLD) {
-  //       sendSms(message)
-  //       alertThresholdStartedAt = null
-  //     }
-  //
-  //     console.log(message)
-	// 		return
-	// 	}
-  //
-  //   alertThresholdStartedAt = null
-	// 	// console.log('Farm profitability:', stringifyProfitability(profitability), ', average:', stringifyProfitability(averageProfitability))
-	// } catch (err) {
-	// 	console.error(err)
-	// }
-
-  // status
+const printRigStats = () => {
   Object.keys(rigs).forEach(x => {
     const rig = rigs[x]
 
-    console.log(`Rig: ${rig.name}`)
+    console.log(`Rig: ${rig.name}`, rig.updatedAt ? `(${rig.updatedAt.fromNow()})` : '')
 
     if (rig.gpus.length) {
       const t = new Table()
       rig.gpus.forEach(gpu => {
+        t.cell('PCI Port', gpu.pci)
         t.cell('GPU', gpu.name)
         t.cell('Usage', gpu.usage)
         t.cell('Temperature', gpu.temperature)
-        t.cell('Power', gpu.power)
-        // t.cell('Price, USD', product.price, Table.number(2))
+        t.cell('Power Draw', gpu.power.draw)
+        t.cell('Power Limit', gpu.power.limit)
+        t.cell('Fan', gpu.power.fan)
         t.newRow()
       })
       console.log(t.toString())
@@ -158,7 +151,17 @@ const main = async () => {
   })
 }
 
+let alertThresholdStartedAt
+
+const main = async () => {
+  clearScreen()
+  printFarmStats()
+  printRigStats()
+}
+
+fetchNicehashStats().then(stats => nicehashStats = stats)
+
 setInterval(main, UPDATE_INTERVAL)
 setInterval(async () => {
-  profitability = await calcProfitability()
-}, NICEHASH_UPDATE_INTERVAL)
+  nicehashStats = await fetchNicehashStats()
+}, NICEHASH_FETCH_INTERVAL)
