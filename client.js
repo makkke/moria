78a6ae366ca54ccf89e4cb8537ffccb296597d49
Viatcheslav Ivanov <slava.eth@gmail.com>
2018-01-21 23:30:33 -0800

    reconnect handling

--- client.js
+++ client.js
@@ -7,25 +7,44 @@ const rig = {
   name: process.env.RIG_NAME,
   profitability: process.env.RIG_PROFITABILITY,
 }
+let interval
+let ws
+let handlingError = false
 
-const ws = new WebSocket('ws://77e3e5f8.ngrok.io')
-ws.on('open', () => {
-  console.info('connected to farm')
-  ws.send(JSON.stringify({ type: 'CONNECTION_SUCCESS', payload: { rig } }))
+const connect = () => {
+  const handleError = () => {
+    if (!handlingError) {
+      console.log('disconnected')
+      handlingError = true
+      clearInterval(interval)
+      setTimeout(() => {
+        handlingError = false
+        connect()
+      }, 5000)
+    }
+  }
 
-  setInterval(() => {
-    // get system data
-    smi((err, data) => {
-      if (err) {
-        ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_FAILURE', payload: { rig, err } }))
-        console.warn(err)
-      }
+  console.log('connecting...')
+  ws = new WebSocket('ws://77e3e5f8.ngrok.io')
+  ws.on('open', () => {
+    console.info('connected to farm')
+    ws.send(JSON.stringify({ type: 'CONNECTION_SUCCESS', payload: { rig } }))
 
-      ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_SUCCESS', payload: { rig, data: data.nvidia_smi_log }}))
-    })
-  }, UPDATE_INTERVAL)
-})
+    interval = setInterval(() => {
+      // get system data
+      smi((err, data) => {
+        if (err) {
+          ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_FAILURE', payload: { rig, err } }))
+          console.warn(err)
+        }
 
-ws.on('message', (data) => {
-  console.log(data)
-})
+        ws.send(JSON.stringify({ type: 'LOAD_SYSTEM_DATA_SUCCESS', payload: { rig, data: data.nvidia_smi_log }}))
+      })
+    }, UPDATE_INTERVAL)
+  })
+
+  ws.on('error', () => handleError())
+  ws.on('close', () => handleError())
+}
+
+connect()
